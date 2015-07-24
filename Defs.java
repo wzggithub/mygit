223473a1d0b56da7e9f337fb4074d4ae73675a76
Wzg <1246613785@qq.com>
2015-07-24 16:02:15 +0800

    rm test

--- Defs.java
+++
@@ -1,7 +0,0 @@
-package com.youku.linktest;
-
-public class Defs {
-	public static final String TRENDS_SCHEMA ="devdiv://sina_profile1";
-	public static final String MENTIONS_SCHEMA ="com.youku.paike://message_private_url";
-	public static final String PARAM_UID ="uid";
-}
