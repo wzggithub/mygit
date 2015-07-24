afda6d3ee59c5c91f5b6876192e1b907c6bf1c0b
Wzg <1246613785@qq.com>
2015-07-24 15:58:59 +0800

    des

---
+++ Defs.java
@@ -0,0 +1,7 @@
+package com.youku.linktest;
+
+public class Defs {
+	public static final String TRENDS_SCHEMA ="devdiv://sina_profile1";
+	public static final String MENTIONS_SCHEMA ="com.youku.paike://message_private_url";
+	public static final String PARAM_UID ="uid";
+}
